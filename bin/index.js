ade1c613a2cdc67a250f4fcf43422e82af6cc140
aric.zheng <1290657123@qq.com>
2025-09-01 17:05:09 +0800

    feat: add run scripts method

--- bin/index.js
+++ bin/index.js
@@ -32,7 +32,15 @@ class CliApp {
   }
 
   run() {
-    this.log('run cli: ', __dirname, this.args, this.opts, pkg.version);
+    // 相当于: node <script> <scriptArgs>
+    // 获取要执行的脚本路径
+    const script = this.args[0];
+    // 获取脚本参数
+    const scriptArgs = this.args.slice(1);
+    // 使用 node 子进程执行脚本，并继承当前终端的输入输出
+    const child = spawn('node', [script, ...scriptArgs], { stdio: 'inherit' });
+    // 子进程退出时，主进程也以相同的退出码退出
+    child.on('exit', (code) => process.exit(code));
   }
 }
 
